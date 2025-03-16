24593893cd3504e7f07c38c680bdf3deb7b9a4e6
신윤호 <xode114kr1@naver.com>
2025-03-17 01:12:56 +0900

    fix : REACT_APP_BACKEND_PROXY

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${React_APP_BACKEND_PROXY}/api`,
+  baseURL: `${REACT_APP_BACKEND_PROXY}/api`,
   headers: {
     "Content-Type": "application/json",
   },
