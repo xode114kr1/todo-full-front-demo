9d7b54942acee6f3179b508eb46a6446e4cbaf5d
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-03-17 09:53:06 +0900

    Update api.js
    fix : url

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `${REACT_APP_BACKEND_PROXY}/api`,
+  baseURL: `${process.env.REACT_APP_BACKEND_PROXY}/api`,
   headers: {
     "Content-Type": "application/json",
   },
